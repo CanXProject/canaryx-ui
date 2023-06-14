8ce6672a844b59809cb2eb775fd56c073d2ce8cb
CanaryXDAO <95591343+CanaryXDAO@users.noreply.github.com>
2023-06-14 18:39:23 -0400

    Updated Bifrost & CAND logo and page meta description/open graph

--- apps/web/next-seo.config.ts
+++ apps/web/next-seo.config.ts
@@ -11,9 +11,9 @@ export const SEO: DefaultSeoProps = {
     site: '@canaryxtoken',
   },
   openGraph: {
-    title: 'CanarySwap - Unlock the world of CanaryX | Mint Founder NFTs',
+    title: '💎 CanaryX - Revolutionizing the songbird Network!',
     description:
-      'The most popular AMM on BSC by user count! Earn CAKE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by PancakeSwap), NFTs, and more, on a platform you can trust.',
+      'Join the revolution - CanaryX on Songbird! Engage in seamless token swaps, provide liquidity for rewards, and earn $CANARY through exciting yield farming opportunities. Enter the new era of decentralized finance, on a platform you can trust.',
     images: [{ url: 'https://pbs.twimg.com/media/FcEa3ryXkAAGPB7?format=png&name=small' }],
   },
 }
